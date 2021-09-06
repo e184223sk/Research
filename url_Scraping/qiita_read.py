fd5a489b12ba26f7a48c34081412485bd7b57a75
e184223sk <e184223sk@kurume-it.ac.jp>
2021-09-06 13:00:38 +0900

    comment

--- url_Scraping/qiita_read.py
+++ url_Scraping/qiita_read.py
@@ -7,8 +7,15 @@ conn = http.client.HTTPSConnection("qiita.com")
 url = "/api/v2/tags/csharp/items?"
 
 
+<<<<<<< HEAD
+# １回につき記事100枚
+# それを100回
+# = 10000枚が１回の実行で取得できる最大数
+for i in range(100):
+=======
 
 for i in range(200):
+>>>>>>> f8f9b6f2256c711478d26eb163c10718a493e9a6
     i += 1
     # Qiita APIで記事情報を取得
     page = "page=" + str(i)
