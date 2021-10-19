ab3cd050d116c094147b84be7447c20c670175af
園田 <e184223sk@kurume-it.ac.jp>
2021-10-19 13:37:16 +0900

    データのシェイプがあってないらしいです

--- ExplainDetecter/ActivateED.bat
+++ ExplainDetecter/ActivateED.bat
@@ -1,3 +1,4 @@
+call C:\Users\konolab\Anaconda3\Scripts\activate.bat
 call activate tensorflow-gpu
 cd C:\Users\konolab\Desktop\Research\ExplainDetecter
-jupyter notebook
+jupyter notebook training.ipynb
